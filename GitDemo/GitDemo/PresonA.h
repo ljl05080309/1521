d80150202f15902d144e9d13fd164d13d556077d
qianfeng0 <qianfeng0@Macintosh-4.local>
2016-04-01 15:11:39 +0800

    B创建一个PersonA类

---
+++ GitDemo/GitDemo/PresonA.h
@@ -0,0 +1,13 @@
+//
+//  PresonA.h
+//  GitDemo
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 LJL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PresonA : NSObject
+
+@end
---
+++ GitDemo/GitDemo/PresonA.m
@@ -0,0 +1,13 @@
+//
+//  PresonA.m
+//  GitDemo
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 LJL. All rights reserved.
+//
+
+#import "PresonA.h"
+
+@implementation PresonA
+
+@end
